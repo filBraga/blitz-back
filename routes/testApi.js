4842318286ebf0a51907b58cdec3f8a687697c02
Filipe <filipe.b.braga@gmail.com>
2022-07-02 09:09:59 -0300

    adding Test Url

--- routes/testApi.js
+++ routes/testApi.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('API works fine. Good job!')
+  res.send('API works fine. Good job!!')
 })
 
 module.exports = router;
